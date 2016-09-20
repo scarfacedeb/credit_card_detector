1548a78a68a2e59433fb9608ed75bea8bfec7c8d
Andrew Volozhanin <scarfacedeb@gmail.com>
2016-09-20 13:32:55 +0300

    Add Luhn specs

---
+++ spec/luhn_spec.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+require "credit_card_detector/luhn"
+
+module CreditCardDetector
+  describe Luhn do
+    describe ".valid?" do
+      it "returns true when number is valid" do
+        LUHN_ENABLED.each do |brand, card_numbers|
+          card_numbers.each do |number|
+            Luhn.valid?(number.to_s.tr("- ", "")).must_equal true
+          end
+        end
+      end
+
+      it "returns false when number is invalid" do
+        Luhn.valid?("4111111111111110").must_equal false
+      end
+    end
+  end
+end
